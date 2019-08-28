322a4e54e30efde44b93b0fdce3d3ae1eed2c07b
wandergis <wandergis@foxmail.com>
2019-08-28 20:06:39 +0800

    use push(...) to replace concat for perfermance

--- geojson-bbox.js
+++ geojson-bbox.js
@@ -1,45 +1,53 @@
 module.exports = function(gj) {
-  var coords, bbox;
+  let coords;
   if (!gj.hasOwnProperty('type')) return;
   coords = getCoordinatesDump(gj);
-  bbox = [ Number.POSITIVE_INFINITY,Number.POSITIVE_INFINITY,
-      Number.NEGATIVE_INFINITY, Number.NEGATIVE_INFINITY,];
-  return coords.reduce(function(prev,coord) {
-    return [
-      Math.min(coord[0], prev[0]),
-      Math.min(coord[1], prev[1]),
-      Math.max(coord[0], prev[2]),
-      Math.max(coord[1], prev[3])
-    ];
-  }, bbox);
+  return coords.reduce(
+    function(prev, coord) {
+      return [
+        Math.min(coord[0], prev[0]),
+        Math.min(coord[1], prev[1]),
+        Math.max(coord[0], prev[2]),
+        Math.max(coord[1], prev[3])
+      ];
+    },
+    [
+      Number.POSITIVE_INFINITY,
+      Number.POSITIVE_INFINITY,
+      Number.NEGATIVE_INFINITY,
+      Number.NEGATIVE_INFINITY
+    ]
+  );
 };
 
 function getCoordinatesDump(gj) {
-  var coords;
+  let coords;
   if (gj.type == 'Point') {
     coords = [gj.coordinates];
   } else if (gj.type == 'LineString' || gj.type == 'MultiPoint') {
     coords = gj.coordinates;
   } else if (gj.type == 'Polygon' || gj.type == 'MultiLineString') {
-    coords = gj.coordinates.reduce(function(dump,part) {
-      return dump.concat(part);
+    coords = gj.coordinates.reduce(function(dump, part) {
+      return dump.push(...part);
     }, []);
   } else if (gj.type == 'MultiPolygon') {
-    coords = gj.coordinates.reduce(function(dump,poly) {
-      return dump.concat(poly.reduce(function(points,part) {
-        return points.concat(part);
-      },[]));
-    },[]);
+    coords = gj.coordinates.reduce(function(dump, poly) {
+      return dump.push(
+        ...poly.reduce(function(points, part) {
+          return points.push(...part);
+        }, [])
+      );
+    }, []);
   } else if (gj.type == 'Feature') {
-    coords =  getCoordinatesDump(gj.geometry);
+    coords = getCoordinatesDump(gj.geometry);
   } else if (gj.type == 'GeometryCollection') {
-    coords = gj.geometries.reduce(function(dump,g) {
-      return dump.concat(getCoordinatesDump(g));
-    },[]);
+    coords = gj.geometries.reduce(function(dump, g) {
+      return dump.push(...getCoordinatesDump(g));
+    }, []);
   } else if (gj.type == 'FeatureCollection') {
-    coords = gj.features.reduce(function(dump,f) {
-      return dump.concat(getCoordinatesDump(f));
-    },[]);
+    coords = gj.features.reduce(function(dump, f) {
+      return dump.push(...getCoordinatesDump(f));
+    }, []);
   }
   return coords;
 }
